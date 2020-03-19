d85b64e470731eb5aaf481f52a859baa269e3923
Nicholas Whittaker <nchlswhttkr@gmail.com>
2020-03-19 18:25:28 +1100

    Publish "Newsletter #2020-03", update template
    * Add subscribe/unsubscribe links to footer
    * Make it look nicer in text-only email form

--- .buildkite/publish-newsletter.sh
+++ .buildkite/publish-newsletter.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# $1    - The receiving mailing list address
-
 set -eu
 
 curl --fail "https://nicholas.cloud/newsletter/$NEWSLETTER_ISSUE/" > email.html
@@ -9,7 +7,7 @@ curl --fail "https://templates.mailchimp.com/services/html-to-text/" --data-urle
 
 curl --fail "https://api.eu.mailgun.net/v3/mailgun.nicholas.cloud/messages" \
     --user "api:$MAILGUN_API_KEY" \
-    --data-urlencode to=$1 \
+    --data-urlencode to=$MAILING_LIST_ADDRESS \
     --data-urlencode "from=Nicholas <noreply@mailgun.nicholas.cloud>" \
     --data-urlencode "subject=Newsletter #$NEWSLETTER_ISSUE from nicholas.cloud" \
     --data-urlencode html@email.html \
--- .buildkite/trigger-newsletter-if-new-issue.sh
+++ .buildkite/trigger-newsletter-if-new-issue.sh
@@ -2,14 +2,15 @@
 
 set -eu
 
-git diff-tree --no-commit-id --name-status -r HEAD | grep "^A\tcontent/newsletter/....-..\.md" | cut -f 2 | sed -n 's;content/newsletter/\(....-..\)\.md;\1;p' > new-newsletters.txt
+git diff-tree --no-commit-id --name-status -r HEAD | grep "^A.content/newsletter/....-..\.md" | cut -f 2 | sed -n 's;content/newsletter/\(....-..\)\.md;\1;p' > new-newsletters.txt
 
 while read NEWSLETTER; do
-    curl -X POST "https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/newsletter/builds" \
+    echo "Publishing Newsletter #$NEWSLETTER"
+    curl --fail "https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/newsletter/builds" \
         -H "Authorization: Bearer $BUILDKITE_API_TOKEN" \
         -d "{
-            \"commit\": \"HEAD\",
-            \"branch\": \"master\",
+            \"commit\": \"$BUILDKITE_COMMIT\",
+            \"branch\": \"$BUILDKITE_BRANCH\",
             \"env\": {
                 \"NEWSLETTER_ISSUE\": \"$NEWSLETTER\"
             }
