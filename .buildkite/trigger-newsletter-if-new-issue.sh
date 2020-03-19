93fc4f261eba0733f440bb0d6cd79edfab4a5881
Nicholas Whittaker <nchlswhttkr@gmail.com>
2020-03-19 17:42:44 +1100

    Debugging

--- .buildkite/trigger-newsletter-if-new-issue.sh
+++ .buildkite/trigger-newsletter-if-new-issue.sh
@@ -2,9 +2,13 @@
 
 set -euo pipefail
 
+echo 'Debugging'
 git diff-tree --no-commit-id --name-status -r HEAD | grep "^A\tcontent/newsletter/....-..\.md" | cut -f 2 | sed -n 's;content/newsletter/\(....-..\)\.md;\1;p' > new-newsletters.txt
+cat new-newsletters.txt
 
+echo 'Looping'
 while read NEWSLETTER; do
+    echo 'Curling'
     curl -X POST "https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/newsletter/builds" \
         -H "Authorization: Bearer $BUILDKITE_API_TOKEN" \
         -d '{
@@ -15,3 +19,4 @@ while read NEWSLETTER; do
             }
         }'
 done < new-newsletters.txt
+echo 'Done'
