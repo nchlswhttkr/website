aa263d06cd0b8a8fcb2b7b01d59ff75a55e8bb84
Nicholas Whittaker <nchlswhttkr@gmail.com>
2020-03-22 16:31:32 +1100

    Fix newsletter publishing missing a block step
    This accidentally sent an email to all my subscribers, oops!

--- .buildkite/trigger-newsletter-if-new-issue.sh
+++ .buildkite/trigger-newsletter-if-new-issue.sh
@@ -4,22 +4,21 @@ set -eu
 
 git diff-tree --no-commit-id --name-status -r HEAD | grep "^A.content/newsletter/....-..\.md" | cut -f 2 | sed -n 's;content/newsletter/\(....-..\)\.md;\1;p' > new-newsletters.txt
 
-echo "2020-03" >> new-newsletters.txt # TEMPORARY, WILL BE REMOVED SOON
+echo "2020-01" >> new-newsletters.txt # TEMPORARY, WILL BE REMOVED SOON
 
 while read NEWSLETTER; do
     echo "
         steps:
           - label: \":email: Preview\"
             command: .buildkite/publish-newsletter.sh
-            key: preview-$NEWSLETTER
             agents:
                 deploy-personal-website: true
             env:
               NEWSLETTER_ISSUE: $NEWSLETTER
               MAILING_LIST_ADDRESS: newsletter-preview@mailgun.nicholas.cloud
+          - block: Publish?
           - label: \":email: Publish\"
             command: .buildkite/publish-newsletter.sh
-            depends_on: preview-$NEWSLETTER
             agents:
                 deploy-personal-website: true
             env:
