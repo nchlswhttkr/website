5f1bfdbf12bbfa25cfa2959209efa30d3d02a46e
Nicholas Whittaker <nchlswhttkr@gmail.com>
2020-03-22 16:10:36 +1100

    Publish newsletter through dynamic pipeline
    It's a bit simpler than having a separate pipeline. Instead the
    email-sending steps are added dynamically.

--- .buildkite/trigger-newsletter-if-new-issue.sh
+++ .buildkite/trigger-newsletter-if-new-issue.sh
@@ -4,15 +4,26 @@ set -eu
 
 git diff-tree --no-commit-id --name-status -r HEAD | grep "^A.content/newsletter/....-..\.md" | cut -f 2 | sed -n 's;content/newsletter/\(....-..\)\.md;\1;p' > new-newsletters.txt
 
+echo "2020-03" >> new-newsletters.txt # TEMPORARY, WILL BE REMOVED SOON
+
 while read NEWSLETTER; do
-    echo "Publishing Newsletter #$NEWSLETTER"
-    curl --fail "https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/newsletter/builds" \
-        -H "Authorization: Bearer $BUILDKITE_API_TOKEN" \
-        -d "{
-            \"commit\": \"$BUILDKITE_COMMIT\",
-            \"branch\": \"$BUILDKITE_BRANCH\",
-            \"env\": {
-                \"NEWSLETTER_ISSUE\": \"$NEWSLETTER\"
-            }
-        }"
+    echo "
+        steps:
+          - label: \":email: Preview\"
+            command: .buildkite/publish-newsletter.sh
+            key: preview-$NEWSLETTER
+            agents:
+                deploy-personal-website: true
+            env:
+              NEWSLETTER_ISSUE: $NEWSLETTER
+              MAILING_LIST_ADDRESS: newsletter-preview@mailgun.nicholas.cloud
+          - label: \":email: Publish\"
+            command: .buildkite/publish-newsletter.sh
+            depends_on: preview-$NEWSLETTER
+            agents:
+                deploy-personal-website: true
+            env:
+              NEWSLETTER_ISSUE: $NEWSLETTER
+              MAILING_LIST_ADDRESS: newsletter@mailgun.nicholas.cloud
+    " | buildkite-agent pipeline upload
 done < new-newsletters.txt
