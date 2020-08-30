acf8686fc2c46fc0c5de088dd5b81ede20185367
Nicholas Whittaker <nicholas@nicholas.cloud>
2020-08-30 13:04:05 +1000

    Publish "Newsletter #2020-08"

--- .buildkite/publish-newsletter.sh
+++ .buildkite/publish-newsletter.sh
@@ -8,7 +8,8 @@ curl --fail "https://templates.mailchimp.com/services/html-to-text/" --data-urle
 curl --fail "https://api.eu.mailgun.net/v3/mailgun.nicholas.cloud/messages" \
     --user "api:$MAILGUN_API_KEY" \
     --data-urlencode to=$MAILING_LIST_ADDRESS \
-    --data-urlencode "from=Nicholas <noreply@mailgun.nicholas.cloud>" \
+    --data-urlencode "from=Nicholas Whittaker <noreply@mailgun.nicholas.cloud>" \
     --data-urlencode "subject=Newsletter #$NEWSLETTER_ISSUE from nicholas.cloud" \
+    --data-urlencode "h:Reply-To=Nicholas Whittaker <nicholas@nicholas.cloud>" \
     --data-urlencode html@email.html \
     --data-urlencode text@email.text
--- .buildkite/trigger-newsletter-if-new-issue.sh
+++ .buildkite/trigger-newsletter-if-new-issue.sh
@@ -2,29 +2,29 @@
 
 set -eu
 
-git diff-tree --no-commit-id --name-status -r HEAD | cut -f 2 | grep "^content/newsletter/....-..\.md" | sed -n 's;content/newsletter/\(....-..\)\.md;\1;p' > new-newsletters.txt
+git diff-tree --no-commit-id --name-status -r HEAD | grep "^[AM]\tcontent/newsletter/....-..\.md"  | cut -f 2 | sed -n "s;content/newsletter/\(....-..\)\.md;\1;p" > new-newsletters.txt
 
 while read NEWSLETTER; do
-    echo "
-        steps:
-          - label: \":email: Preview\"
-            key: preview-newsletter-$NEWSLETTER
-            command: .buildkite/publish-newsletter.sh
-            agents:
-                deploy-personal-website: true
-            env:
-              NEWSLETTER_ISSUE: $NEWSLETTER
-              MAILING_LIST_ADDRESS: newsletter-preview@mailgun.nicholas.cloud
-          - block: \"Publish newsletter #$NEWSLETTER?\"
-            key: approve-newsletter-$NEWSLETTER
-            depends_on: preview-newsletter-$NEWSLETTER
-          - label: \":email: Publish\"
-            depends_on: approve-newsletter-$NEWSLETTER
-            command: .buildkite/publish-newsletter.sh
-            agents:
-                deploy-personal-website: true
-            env:
-              NEWSLETTER_ISSUE: $NEWSLETTER
-              MAILING_LIST_ADDRESS: newsletter@mailgun.nicholas.cloud
-    " | buildkite-agent pipeline upload
+  echo "
+    steps:
+      - label: \":email: Preview\"
+        key: preview-newsletter-$NEWSLETTER
+        command: .buildkite/publish-newsletter.sh
+        agents:
+          deploy-personal-website: true
+        env:
+          NEWSLETTER_ISSUE: $NEWSLETTER
+          MAILING_LIST_ADDRESS: newsletter-preview@mailgun.nicholas.cloud
+      - block: \"Publish newsletter #$NEWSLETTER?\"
+        key: approve-newsletter-$NEWSLETTER
+        depends_on: preview-newsletter-$NEWSLETTER
+      - label: \":email: Publish\"
+        depends_on: approve-newsletter-$NEWSLETTER
+        command: .buildkite/publish-newsletter.sh
+        agents:
+          deploy-personal-website: true
+        env:
+          NEWSLETTER_ISSUE: $NEWSLETTER
+          MAILING_LIST_ADDRESS: newsletter@mailgun.nicholas.cloud
+  " | buildkite-agent pipeline upload
 done < new-newsletters.txt
