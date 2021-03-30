3a1299ac04d1d69e20e0513857f59c74b8c990ec
Nicholas Whittaker <nicholas@nicholas.cloud>
2021-03-31 00:22:02 +1100

    Update Buildkite tags

--- .buildkite/trigger-newsletter-if-new-issue.sh
+++ .buildkite/trigger-newsletter-if-new-issue.sh
@@ -11,7 +11,7 @@ while read NEWSLETTER; do
         key: preview-newsletter-$NEWSLETTER
         command: .buildkite/publish-newsletter.sh
         agents:
-          deploy-personal-website: true
+          website: nicholas.cloud
         env:
           NEWSLETTER_ISSUE: $NEWSLETTER
           MAILING_LIST_ADDRESS: newsletter-preview@mailgun.nicholas.cloud
@@ -22,7 +22,7 @@ while read NEWSLETTER; do
         depends_on: approve-newsletter-$NEWSLETTER
         command: .buildkite/publish-newsletter.sh
         agents:
-          deploy-personal-website: true
+          website: nicholas.cloud
         env:
           NEWSLETTER_ISSUE: $NEWSLETTER
           MAILING_LIST_ADDRESS: newsletter@mailgun.nicholas.cloud
