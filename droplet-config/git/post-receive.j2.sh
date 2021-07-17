584855d88f2fc30c397aebd237202b07efb03247
Nicholas Whittaker <nicholas@nicholas.cloud>
2021-07-17 17:26:36 +1000

    Improve mirrored Git project install process
    * Query GitHub for repos to mirror, install any missing locally
    * Grab mandarin-duck version from existing install, rather than a fact

--- droplet-config/git/post-receive.j2.sh
+++ droplet-config/git/post-receive.j2.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 REPO=$(pwd | cut -d '/' -f 5)
 cd "../${REPO%%.git}"
 stagit -u "https://nicholas.cloud/git/${REPO%%.git}/" "../$REPO"
-stagit-index $(find /var/www/git -mindepth 1 -maxdepth 1 -name "*.git") > /var/www/git/index.html
+stagit-index "$(find /var/www/git -mindepth 1 -maxdepth 1 -name '*.git')" > /var/www/git/index.html
 
-# shellcheck disable=SC1009,SC1054,SC1056,SC1072,SC1073,SC1083
-{% if item.mandarin_duck_enabled %}
-source ~/.mandarin-duck/post-receive.sh
-{% endif %}
+# Only trigger a build if repo is registered to mandarin-duck
+if jq --exit-status ".projects[\"$PWD\"]" ~/.mandarin-duck/mandarin-duck.cfg > /dev/null; then
+    source ~/.mandarin-duck/post-receive.sh
+fi
