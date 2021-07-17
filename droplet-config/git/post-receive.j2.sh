1a854881928d831b030b785f0a3ffe524599e8b4
Nicholas Whittaker <nicholas@nicholas.cloud>
2021-07-17 23:36:01 +1000

    Fix push-receive, allow pushes from mirror job

--- droplet-config/git/post-receive.j2.sh
+++ droplet-config/git/post-receive.j2.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 REPO=$(pwd | cut -d '/' -f 5)
 cd "../${REPO%%.git}"
 stagit -u "https://nicholas.cloud/git/${REPO%%.git}/" "../$REPO"
-stagit-index "$(find /var/www/git -mindepth 1 -maxdepth 1 -name '*.git')" > /var/www/git/index.html
+# shellcheck disable=SC2046
+stagit-index $(find /var/www/git -mindepth 1 -maxdepth 1 -name '*.git') > /var/www/git/index.html
 
 # Only trigger a build if repo is registered to mandarin-duck
 if jq --exit-status ".projects[\"$PWD\"]" ~/.mandarin-duck/mandarin-duck.cfg > /dev/null; then
