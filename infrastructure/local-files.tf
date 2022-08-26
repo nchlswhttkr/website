88957757ba4f7559d6b16d46499f2612c893694d
Nicholas Whittaker <nicholas@nicholas.cloud>
2022-08-27 03:20:15 +1000

    Don't commit build cache, use new proxy

--- infrastructure/local-files.tf
+++ infrastructure/local-files.tf
@@ -1,12 +1,3 @@
-data "pass_password" "embed_proxy_secret" {
-  name = "website/embed-proxy-secret.txt"
-}
-
-resource "local_sensitive_file" "embed_proxy_secret" {
-  content  = data.pass_password.embed_proxy_secret.password
-  filename = "../secrets/embed-proxy-secret.txt"
-}
-
 data "pass_password" "remote_user_key" {
   name = "website/remote-user.pem"
 }
