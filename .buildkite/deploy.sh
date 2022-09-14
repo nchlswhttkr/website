b1010744bd480b89cc8585b533408c9d41f309c2
Nicholas Whittaker <nicholas@nicholas.cloud>
2022-09-14 19:18:28 +1000

    Fix artifact download command

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 echo --- Deploying nicholas.cloud
-buildkite-agent artifact download website.tar.gz --step build
+buildkite-agent artifact download website.tar.gz . --step build
 tar -xz -f website.tar.gz
 rsync --recursive --verbose --itemize-changes --checksum --delete "$PWD/public/" --exclude "files" /var/www/nicholas.cloud
