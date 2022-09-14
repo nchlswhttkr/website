d294f8851ea0e1428506cc9dd880720c0ca491f3
Nicholas Whittaker <nicholas@nicholas.cloud>
2022-09-14 19:11:18 +1000

    Remove Hugo-related setup from deploy playbooks

--- deploy/roles/buildkite/files/download-hugo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# $1 - Hugo version number, for example 0.92.0
-
-set -euo pipefail
-
-curl --silent --fail --location "https://github.com/gohugoio/hugo/releases/download/v$1/hugo_$1_Linux-64bit.tar.gz" \
-    | tar --gzip --extract --directory="$HOME" hugo
