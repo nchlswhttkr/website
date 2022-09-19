a0c0ff2e9bba6f1c7a9fbbbdecb658e7b5a56a60
Nicholas Whittaker <nicholas@nicholas.cloud>
2022-09-19 19:24:12 +1000

    Run all non-deploy Buildkite steps on AWS

--- .buildkite/trigger-newsletter-if-new-issue.sh
+++ .buildkite/trigger-newsletter-if-new-issue.sh
@@ -13,8 +13,6 @@ while read -r NEWSLETTER < new-newsletters.txt; do
       - label: \":email: Preview newsletter #$NEWSLETTER\"
         key: preview-newsletter-$NEWSLETTER
         command: .buildkite/publish-newsletter.sh
-        agents:
-          website: nicholas.cloud
         env:
           NEWSLETTER_ISSUE: $NEWSLETTER
           MAILING_LIST_ADDRESS: newsletter-preview@mailgun.nicholas.cloud
@@ -22,10 +20,9 @@ while read -r NEWSLETTER < new-newsletters.txt; do
         key: approve-newsletter-$NEWSLETTER
         depends_on: preview-newsletter-$NEWSLETTER
       - label: \":email: Publish newsletter #$NEWSLETTER\"
+        key: publish-newsletter-$NEWSLETTER
         depends_on: approve-newsletter-$NEWSLETTER
         command: .buildkite/publish-newsletter.sh
-        agents:
-          website: nicholas.cloud
         env:
           NEWSLETTER_ISSUE: $NEWSLETTER
           MAILING_LIST_ADDRESS: newsletter@mailgun.nicholas.cloud
