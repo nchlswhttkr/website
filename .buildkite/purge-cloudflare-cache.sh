4634b93b034445ffcf4ff53ad831ac5b34f84e5c
Nicholas Whittaker <nicholas@nicholas.cloud>
2023-07-01 13:09:49 +1000

    Read credentials from Vault

--- .buildkite/purge-cloudflare-cache.sh
+++ .buildkite/purge-cloudflare-cache.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+CLOUDFLARE_API_TOKEN="$(vault kv get -mount=kv -field cloudflare_api_token buildkite/website)"
+CLOUDFLARE_ZONE_ID="$(vault kv get -mount=kv -field cloudflare_zone_id buildkite/website)"
+
 curl --silent --fail "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache" \
     -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
     -H "Content-Type: application/json" \
