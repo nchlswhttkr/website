af950513add9cb632d1e0b0d6c781cb92075a300
Nicholas Whittaker <nicholas@nicholas.cloud>
2023-07-09 00:10:18 +1000

    Update rsync path for deploys

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 echo --- Deploying nicholas.cloud
 buildkite-agent artifact download website.tar.gz . --step build
 tar -xz -f website.tar.gz
-rsync --recursive --verbose --itemize-changes --checksum --delete "$PWD/public/" --exclude "files" -e "ssh -o StrictHostKeyChecking=no" nicholas@gandra-dee:/var/www/nicholas.cloud
+rsync --recursive --verbose --itemize-changes --checksum --delete "$PWD/public/" --exclude "files" -e "ssh -o StrictHostKeyChecking=no" website@gandra-dee:/home/website/nicholas.cloud
