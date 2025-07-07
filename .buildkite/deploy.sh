632d142871c7bbc187378f8eba4ffc6d6947e5d7
Nicholas Whittaker <nicholas@nicholas.cloud>
2025-07-07 18:31:11 +1000

    Support dynamic deployment targets (#56)

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -5,4 +5,6 @@ set -euo pipefail
 echo --- Deploying nicholas.cloud
 buildkite-agent artifact download website.tar.gz . --step build
 tar -xz -f website.tar.gz
-rsync --recursive --verbose --itemize-changes --checksum --delete "$PWD/public/" --exclude "files" -e "ssh -o StrictHostKeyChecking=no" blog@blog:/var/www/nicholas.cloud
+tailscale status --json | jq --raw-output '.Peer.[] | select(.Tags // [] | any(. == "tag:project-blog")) | .DNSName' | while read -r HOST; do
+    rsync --recursive --verbose --itemize-changes --checksum --delete "$PWD/public/" --exclude "files" -e "ssh -o StrictHostKeyChecking=no" "blog@$HOST:/var/www/nicholas.cloud"
+done;
