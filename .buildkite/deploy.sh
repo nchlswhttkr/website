e886dc64726090571a554e0ff56b01cd0149641e
Nicholas Whittaker <nicholas@nicholas.cloud>
2025-07-07 18:28:51 +1000

    Fix deploy command

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 echo --- Deploying nicholas.cloud
 buildkite-agent artifact download website.tar.gz . --step build
 tar -xz -f website.tar.gz
-tailscale status --json | jq '.Peer.[] | select(.Tags // [] | any(. == "tag:project-blog")) | .DNSName' | while read -r HOST; do
+tailscale status --json | jq --raw-output '.Peer.[] | select(.Tags // [] | any(. == "tag:project-blog")) | .DNSName' | while read -r HOST; do
     rsync --recursive --verbose --itemize-changes --checksum --delete "$PWD/public/" --exclude "files" -e "ssh -o StrictHostKeyChecking=no" "blog@$HOST:/var/www/nicholas.cloud"
 done;
